fcbfc4d349cecaf080a96807b175dd4895ac4063
Justin Gist <justin.gist@smail.astate.edu>
2019-08-29 15:09:22 -0500

    Added some stupid comments for my first commit

---
+++ testgithub/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  testgithub
+//
+//  Created by Justin Gist on 8/29/19.
+//  Copyright © 2019 Jus Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // please work dood
+    // pls dud
+
+}
+
